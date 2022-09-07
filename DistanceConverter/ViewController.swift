97f9ec667570baa0327a6822cbe13afa4e3228f0
b-knd <angjingru@gmail.com>
2022-09-07 11:08:57 +0800

    Added another unit for conversion and touch up on the interface by adding title and a simple instruction as subtitle

--- DistanceConverter/ViewController.swift
+++ DistanceConverter/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var distance = Distance(miles: 1000)
+    var distance = Distance(km: 1)
     
     @IBOutlet weak var milesTextField: UITextField!
     
@@ -16,20 +16,23 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var yardTextField: UITextField!
     
+    @IBOutlet weak var feetTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        milesTextField.text = "\(distance.miles)"
-        kmTextField.text = "\(distance.km)"
-        yardTextField.text = "\(distance.yard)"
+        kmTextField.text = "\(Double(round(distance.km*100)/100))"
+        milesTextField.text = "\(Double(round(distance.miles*100)/100))"
+        yardTextField.text = "\(Double(round(distance.yard*100)/100))"
+        feetTextField.text = "\(Double(round(distance.feet*100)/100))"
         
         view.backgroundColor = UIColor(red: 0.146, green: 0.180, blue: 0.236, alpha: 1.0)
         
         milesTextField.addTarget(self, action: #selector(milesConversion), for: .editingChanged)
         kmTextField.addTarget(self, action: #selector(kmConversion), for: .editingChanged)
         yardTextField.addTarget(self, action: #selector(yardConversion), for: .editingChanged)
+        feetTextField.addTarget(self, action: #selector(feetConversion), for: .editingChanged)
     }
     
     @objc func milesConversion(_ sender: Any) {
@@ -45,6 +48,7 @@ class ViewController: UIViewController {
             distance.km = km
             yardTextField.text = "\(Double(round(distance.yard*100)/100))"
             milesTextField.text = "\(Double(round(distance.miles*100)/100))"
+            feetTextField.text = "\(Double(round(distance.feet*100)/100))"
         }
     }
     
@@ -53,6 +57,16 @@ class ViewController: UIViewController {
             distance.yard = yard
             milesTextField.text = "\(Double(round(distance.miles*100)/100))"
             kmTextField.text = "\(Double(round(distance.km*100)/100))"
+            feetTextField.text = "\(Double(round(distance.feet*100)/100))"
+        }
+    }
+    
+    @objc func feetConversion(_ sender: Any) {
+        if let feet = Double(feetTextField.text!){
+            distance.feet = feet
+            milesTextField.text = "\(Double(round(distance.miles*100)/100))"
+            kmTextField.text = "\(Double(round(distance.km*100)/100))"
+            yardTextField.text = "\(Double(round(distance.yard*100)/100))"
         }
     }
     
